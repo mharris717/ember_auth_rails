e1ba2a0c5a4d0567943c7f4552bccffefe39ab7b
Mike Harris <mharris717@gmail.com>
2013-06-12 12:48:03 -0400

    allow user lookup by auth token

--- app/controllers/ember_users_controller.rb
+++ app/controllers/ember_users_controller.rb
@@ -1,7 +1,13 @@
 class EmberUsersController < ApplicationController
   def show
     Rails.logger.info "EmberUsers#show getting user #{params[:id]}"
-    @user = User.find(params[:id])
+    raise "no user id" unless params[:id].present?
+    if params[:id].to_s.length >= 10
+      @user = User.where(:authentication_token => params[:id]).first
+    else
+      @user = User.find(params[:id])
+    end
+    raise "no user found for #{params[:id]}" unless @user
     render :json => @user
   end
 end
