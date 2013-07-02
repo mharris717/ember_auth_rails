8533786ba32bb5ad43d0d4cfde21fe13c7534254
Mike Harris <mharris717@gmail.com>
2013-07-02 10:23:56 -0400

    update password action for ember_users

--- app/controllers/ember_users_controller.rb
+++ app/controllers/ember_users_controller.rb
@@ -1,13 +1,30 @@
 class EmberUsersController < ApplicationController
+  def get_user_by_ident(ident)
+    raise "no user ident" unless ident.present?
+    if ident.to_s.length >= 10
+      User.where(:authentication_token => ident).first
+    else
+      User.find(ident)
+    end
+  end
+
   def show
     Rails.logger.info "EmberUsers#show getting user #{params[:id]}"
-    raise "no user id" unless params[:id].present?
-    if params[:id].to_s.length >= 10
-      @user = User.where(:authentication_token => params[:id]).first
-    else
-      @user = User.find(params[:id])
-    end
+    @user = get_user_by_ident(params[:id])
     raise "no user found for #{params[:id]}" unless @user
     render :json => @user
+  end
+
+  def update
+    authenticate_ember_user!
+    @user = get_user_by_ident(params[:id])
+    new_password = params[:user].andand[:password]
+    raise "no password" unless new_password.present?
+    @user.password = new_password
+    if @user.save
+      render :json => @user
+    else
+      render :json => {:error => "Password " + @user.errors.values.flatten.join(", ")}
+    end
   end
 end
